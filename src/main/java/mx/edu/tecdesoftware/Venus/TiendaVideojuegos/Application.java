ae0bd117adf228e080a90fef8a4a67dd92df8ca7
Semino Alemán Venus Getsemaní <venus.semino@tecdesoftware>
2026-07-20 08:08:07 -0600

    chore: initial Spring Boot project setup

---
+++ src/main/java/mx/edu/tecdesoftware/Venus/TiendaVideojuegos/Application.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.Venus.TiendaVideojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
